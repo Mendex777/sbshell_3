d21aea9f499a9ad6d9e090481e3cc2f5cdd42814
Mendex777 <30417083+Mendex777@users.noreply.github.com>
2025-05-26 19:43:29 +0300

    Update check_environment.sh

--- debian/check_environment.sh
+++ debian/check_environment.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+# Проверка: выполняется ли скрипт от имени root
 if [ "$(id -u)" != "0" ]; then
-    echo "错误: 此脚本需要 root 权限"
+    echo "Ошибка: этот скрипт требует прав root"
     exit 1
 fi
 
+# Проверка, установлен ли sing-box
 if command -v sing-box &> /dev/null; then
     current_version=$(sing-box version | grep 'sing-box version' | awk '{print $3}')
-    echo "sing-box 已安装，版本：$current_version"
+    echo "sing-box уже установлен, версия: $current_version"
 else
-    echo "sing-box 未安装"
+    echo "sing-box не установлен"
 fi
 
+# Проверка включения IP-перенаправления для IPv4 и IPv6
 ipv4_forward=$(sysctl net.ipv4.ip_forward | awk '{print $3}')
 ipv6_forward=$(sysctl net.ipv6.conf.all.forwarding | awk '{print $3}')
 
 if [ "$ipv4_forward" -eq 1 ] && [ "$ipv6_forward" -eq 1 ]; then
-    echo "IP 转发已开启"
+    echo "IP-перенаправление уже включено"
 else
-    echo "开启 IP 转发..."
+    echo "Включение IP-перенаправления..."
     sudo sed -i '/net.ipv4.ip_forward/s/^#//;/net.ipv6.conf.all.forwarding/s/^#//' /etc/sysctl.conf
     sudo sysctl -p
-    echo "IP 转发已成功开启"
+    echo "IP-перенаправление успешно включено"
 fi
