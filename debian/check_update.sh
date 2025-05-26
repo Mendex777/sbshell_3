eeec50bd607b2fd464f0884cc6a0adfe39f70d86
Mendex777 <30417083+Mendex777@users.noreply.github.com>
2025-05-26 19:44:26 +0300

    Update check_update.sh

--- debian/check_update.sh
+++ debian/check_update.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 
+# Определение цветов для вывода
 CYAN='\033[0;36m'
 RED='\033[0;31m'
-NC='\033[0m' # No Color
+NC='\033[0m' # Без цвета
 
-echo "正在检测sing-box最新版本..."
+echo "Проверка последней версии sing-box..."
 sudo apt-get update -qq > /dev/null 2>&1
 
+# Проверка, установлен ли sing-box
 if command -v sing-box &> /dev/null; then
     current_version=$(sing-box version | grep 'sing-box version' | awk '{print $3}')
-    echo -e "${CYAN}当前安装的sing-box版本为:${NC} $current_version"
+    echo -e "${CYAN}Текущая установленная версия sing-box:${NC} $current_version"
     
     stable_version=$(apt-cache policy sing-box | grep Candidate | awk '{print $2}')
     beta_version=$(apt-cache policy sing-box-beta | grep Candidate | awk '{print $2}')
     
-    echo -e "${CYAN}稳定版最新版本：${NC} $stable_version"
-    echo -e "${CYAN}测试版最新版本：${NC} $beta_version"
+    echo -e "${CYAN}Последняя стабильная версия:${NC} $stable_version"
+    echo -e "${CYAN}Последняя тестовая версия:${NC} $beta_version"
     
     while true; do
-        read -rp "是否切换版本(1: 稳定版, 2: 测试版） (当前版本: $current_version, 回车取消操作): " switch_choice
+        read -rp "Хотите переключиться на другую версию? (1: стабильная, 2: тестовая) (текущая: $current_version, Enter — отмена): " switch_choice
         case $switch_choice in
             1)
-                echo "选择了切换到稳定版"
+                echo "Вы выбрали переключение на стабильную версию"
                 sudo apt-get install sing-box -y
                 break
                 ;;
             2)
-                echo "选择了切换到测试版"
+                echo "Вы выбрали переключение на тестовую версию"
                 sudo apt-get install sing-box-beta -y
                 break
                 ;;
             '')
-                echo "不进行版本切换"
+                echo "Отмена переключения версии"
                 break
                 ;;
             *)
-                echo -e "${RED}无效的选择，请输入 1 或 2。${NC}"
+                echo -e "${RED}Неверный выбор. Введите 1 или 2.${NC}"
                 ;;
         esac
     done
 else
-    echo -e "${RED}sing-box 未安装${NC}"
+    echo -e "${RED}sing-box не установлен${NC}"
 fi
