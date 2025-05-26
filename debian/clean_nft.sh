6c3ffc9650fc9131a4c1403239dae4973cc7fc79
Mendex777 <30417083+Mendex777@users.noreply.github.com>
2025-05-26 19:44:51 +0300

    Update clean_nft.sh

--- debian/clean_nft.sh
+++ debian/clean_nft.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 清理防火墙规则并停止服务
+# Очистка правил брандмауэра и остановка сервиса
 sudo systemctl stop sing-box
 nft flush ruleset
 
-echo "sing-box 服务已停止,防火墙规则已清理."
+echo "Служба sing-box остановлена, правила брандмауэра очищены."
